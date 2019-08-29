5bcb324b6ede7e2293be38fad2285e6857aa33d3
Renato01011 <40170602+Renato01011@users.noreply.github.com>
2019-08-29 17:59:25 -0500

    feat(Functions_2): Adding Second Set of Functions
    Adding:
    operator<<
    operator[]
    first
    back
    clear
    sort
    reverse

--- main.cpp
+++ main.cpp
@@ -6,15 +6,18 @@ int main() {
 	
 	ForwardList<int> fl;
 	cout << fl.empty() << endl;
-	fl.push_back(10);
 	fl.push_back(20);
-	fl.push_front(30);
-	fl.push_front(40);
-	fl.print();
+	fl.push_back(50);
+	fl.push_front(3);
+	fl.push_front(49);
+	fl.sort();
+	cout << fl;
 	fl.pop_front();
 	fl.pop_back();
-	fl.print();
+	cout << fl;
 	cout << fl.empty() << endl;
 	cout << fl.size() << endl;
-	
+	cout << fl[1] << endl;
+	fl.reverse();
+	cout << fl;
 }
